b296c223d0ea18ad68fc93e926cc8bb91c0c67ee
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-08-24 17:54:59 -0300

    criando componente botão

--- src/components/button/ButtonComponent.jsx
+++ src/components/button/ButtonComponent.jsx
@@ -1,9 +1,18 @@
-export const ButtonComponent = () => {
+import { PropTypes } from "prop-types"
+
+export const ButtonComponent = ({onClick, disabled, loading, children}) => {
   return (
-    <div>
-      
-    </div>
+    <button onClick={onClick} disabled={disabled}>
+        { loading ? 'Carregando' : children }
+    </button>
   )
+}
+
+ButtonComponent.propTypes = {
+    children: PropTypes.node.isRequired,
+    loading: PropTypes.bool,
+    onClick: PropTypes.func,
+    disabled: PropTypes.bool,
 }
 
 
